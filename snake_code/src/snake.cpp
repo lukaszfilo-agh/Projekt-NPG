a449c1566a010dc75d2ff0c861e8684c8d06b316
lukaszfilo-agh <lukaszfilo@student.agh.edu.pl>
2022-05-06 10:50:39 +0200

    Update snake.cpp

--- snake_code/src/snake.cpp
+++ snake_code/src/snake.cpp
@@ -3,5 +3,19 @@
 
 void menu() {
     std::cout << "Witaj w grze snake" << std::endl;
+    std::cout << "MENU GLOWNE" << std::endl;
+    std::cout << "1. Nowa Gra" << std::endl;
+    std::cout << "2. Poziom Trudnosci" << std::endl;
 
+    std::cout << "Wybierz opcje z menu: ";
+    int wybor = 0;
+    std::cin >> wybor;
+    if (wybor == 1)
+    {
+
+    }
+    else if (wybor == 2)
+    {
+
+    }
 }
