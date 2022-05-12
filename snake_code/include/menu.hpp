6f3004ad68642c185f0cd5876eb1d520f82acade
lukaszfilo-agh <lukaszfilo@student.agh.edu.pl>
2022-05-12 17:09:30 +0200

    koncept menu zawartego w klasie

--- snake_code/include/menu.hpp
+++ snake_code/include/menu.hpp
@@ -7,7 +7,17 @@
 #include <conio.h>
 #include <functional>
 
-
+class Menu_Data {
+public:
+    Menu_Data(std::string str = "", int number = 0) : str_(str), int_(number) {};
+    std::string get_str() { return str_;}
+    int get_int() { return int_;}
+    void set_str(std:: string str) { str_ = str;}
+    void set_int(int number) {int_ = number;}
+private:
+    std::string str_;
+    int int_;
+};
 
 void signs();
 
