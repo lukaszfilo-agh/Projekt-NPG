831afe0fcebe5c4db63dd5299a135ac1bda140bd
lukaszfilo-agh <lukaszfilo@student.agh.edu.pl>
2022-05-25 00:20:59 +0200

    naprawienie zjadania owocow i porzadki w kodzie

--- snake_code/src/game.cpp
+++ snake_code/src/game.cpp
@@ -1,39 +1,34 @@
 #include <unistd.h>
 #include "game.hpp"
 
-void board(const Snake& snake, const MenuData& menuData, Fruit& fruit){
-        COORD snake_pos = snake.snake_get_position();
+void board(const Snake& snake, const MenuData& menuData, Fruit& fruit) {
+    COORD snake_pos = snake.snake_get_position();
 
-        menuData.print_score();
+    menuData.print_score();
 
-        for(int i = 0; i < menuData.get_size_y(); i++)
-        {
-            std::cout << "\t\t" + menuData.get_board_chars(menuData.get_board());
-            for(int j = 0; j < menuData.get_size_x() - 2; j++)
-            {
-                if(i == 0 || i == menuData.get_size_y() - 1) std::cout << menuData.get_board_chars(menuData.get_board());
+    for (int i = 0; i < menuData.get_size_y(); i++) {
+        std::cout << "\t\t" + menuData.get_board_chars(menuData.get_board());
+        for (int j = 0; j < menuData.get_size_x() - 2; j++) {
+            if (i == 0 || i == menuData.get_size_y() - 1) std::cout << menuData.get_board_chars(menuData.get_board());
 
-                else if(i == snake_pos.Y && j + 1 == snake_pos.X) std::cout << '0';
-                else if(i == fruit.fruit_get_position().X && j+1 == fruit.fruit_get_position().Y) std::cout << '@';
+            else if (i == snake_pos.Y && j + 1 == snake_pos.X) std::cout << '0';
+            else if (i == fruit.fruit_get_position().Y && j + 1 == fruit.fruit_get_position().X) std::cout << '@';
 
-                else
-                {
-                    bool isBodyPart = false;
-                    for(int k = 0; k < snake.get_body_size() - 1; k++)
-                    {
-                        if(i == snake.get_body_elem(k).Y && j + 1 == snake.get_body_elem(k).X)
-                        {
-                            std::cout << 'o';
-                            isBodyPart = true;
-                            break;
-                        }
+            else {
+                bool isBodyPart = false;
+                for (int k = 0; k < snake.get_body_size() - 1; k++) {
+                    if (i == snake.get_body_elem(k).Y && j + 1 == snake.get_body_elem(k).X) {
+                        std::cout << 'o';
+                        isBodyPart = true;
+                        break;
                     }
-
-                    if(!isBodyPart) std::cout << ' ';
                 }
+
+                if (!isBodyPart) std::cout << ' ';
             }
-            std::cout << menuData.get_board_chars(menuData.get_board()) + "\n";
         }
+        std::cout << menuData.get_board_chars(menuData.get_board()) + "\n";
+    }
 }
 
 void game(MenuData& menuData, Snake& snake, Fruit& fruit) {
@@ -45,23 +40,28 @@ void game(MenuData& menuData, Snake& snake, Fruit& fruit) {
     clear_console();
     console_cursor(false);
 
-    while(!game_over){
+    while (!game_over) {
         board(snake, menuData, fruit);
-        if(kbhit())
-        {
-            switch(getch())
-            {
-                case 'w': snake.snake_direction('u'); break;
-                case 'a': snake.snake_direction('l'); break;
-                case 's': snake.snake_direction('d'); break;
-                case 'd': snake.snake_direction('r'); break;
+        if (kbhit()) {
+            switch (getch()) {
+                case 'w':
+                    snake.snake_direction('u');
+                    break;
+                case 'a':
+                    snake.snake_direction('l');
+                    break;
+                case 's':
+                    snake.snake_direction('d');
+                    break;
+                case 'd':
+                    snake.snake_direction('r');
+                    break;
             }
         }
 
-        if(snake.snake_collided(menuData)) game_over = true;
+        if (snake.snake_collided(menuData)) game_over = true;
 
-        if(snake.snake_eaten(fruit))
-        {
+        if (snake.snake_eaten(fruit)) {
             fruit.fruit_generate(menuData);
             snake.snake_grow();
             menuData.score_add();
