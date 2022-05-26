5e7803fdd07ff7433db90255064c78feeccd7998
lukaszfilo-agh <lukaszfilo@student.agh.edu.pl>
2022-05-26 21:34:12 +0200

    zmiana nazewnictwa funkcji i porzadki w kodzie

--- snake_code/include/snake.hpp
+++ snake_code/include/snake.hpp
@@ -19,15 +19,9 @@ public:
 
     ~Snake() = default;
 
-    void snake_move(); //poruszanie snakem
-
     int get_length() const { return length_; }
 
-    void snake_grow() { length_++; };   //wydluzanie weza
-
-    COORD snake_get_position() const { return position_; };   //uzyskanie pozycji
-
-    void snake_direction(const char direction) { this->direction_ = direction; }; //przypisanie kierunku weza
+    COORD get_position() const { return position_; };   //uzyskanie pozycji
 
     std::vector<COORD> get_body() const { return body_; }
 
@@ -35,9 +29,15 @@ public:
 
     int get_body_size() const { return body_.size(); }
 
-    bool snake_eaten(const Fruit& fruit);
+    bool eaten(const Fruit& fruit);
 
-    bool snake_collided(const MenuData& menuData);    //czy snake zderzyl sie
+    bool collided(const MenuData& menuData);    //czy snake zderzyl sie
+
+    void direction(const char direction) { this->direction_ = direction; }; //przypisanie kierunku weza
+
+    void grow() { length_++; };   //wydluzanie weza
+
+    void move(); //poruszanie snakem
 };
 
 #endif
