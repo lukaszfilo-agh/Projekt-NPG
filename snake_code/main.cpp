c888a4aba690745a47a0e623eff315eb3e8ccea3
Filip Dymczyk <filip.dymczyk@o2.pl>
2022-05-29 22:14:19 +0200

    Wyswietlanie instrukcji oblsugi menu

--- snake_code/main.cpp
+++ snake_code/main.cpp
@@ -3,8 +3,9 @@
 
 int main() {
     ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
-    MenuData menuData; // obiekt przechowujacy dane menu
-    welcome_message();
+    MenuData menuData;        // obiekt przechowujacy dane menu
+    welcome_message();        // wiadomosc powitalna
+    instructions_menu();      // instrukcje obslugi menu
     game_main(menuData);
     return 0;
 }
